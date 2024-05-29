202fd67facac5d542a4b2f3421d967040233ad81
maggyy666 <kczyz20@gmail.com>
2024-05-29 12:25:53 +0200

    Create file that converts datetime

---
+++ Pociag/CustomDateTimeConverter.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace Pociag
+{
+    internal class CustomDateTimeConverter : JsonConverter<DateTime>
+    {
+        public override DateTime Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+        {
+            string dateString = reader.GetString();
+            if (DateTime.TryParse(dateString, out DateTime result))
+            {
+                return result;
+            }
+            else
+            {
+                // Handle parse failure, e.g., return DateTime.MinValue or throw an exception
+                return DateTime.MinValue;
+            }
+        }
+
+        public override void Write(Utf8JsonWriter writer, DateTime value, JsonSerializerOptions options)
+        {
+            writer.WriteStringValue(value.ToString("yyyy-MM-ddTHH:mm:ss"));
+        }
+    }
+}
