5f448a60a270bbe7084126896f9ba31f9da7cb8b
maggyy666 <kczyz20@gmail.com>
2024-05-29 12:26:17 +0200

    Create class for Travel.cs

---
+++ Pociag/Travel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Pociag
+{
+    public class Travel
+    {
+        public string Beginning { get; set; }
+        public string Final { get; set; }
+        public string Status { get; set; }
+        public DateTime Date { get; set; }
+    }
+}
