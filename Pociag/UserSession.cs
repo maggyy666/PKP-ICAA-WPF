745a95b669e99719bbf0976ecb80db15b0aa8e08
maggyy666 <kczyz20@gmail.com>
2024-06-14 23:14:50 +0200

    Create class for User Session

---
+++ Pociag/UserSession.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pociag
+{
+    class UserSession
+    {
+        public static string Username { get; set; } = "Guest";
+        public static bool IsLoggedIn { get; set; } = false;
+        public static void Login(string username)
+        {
+            Username = username;
+            IsLoggedIn = true;
+        }
+        public static void Logout()
+        {
+            Username = "Guest";
+            IsLoggedIn=false;
+        }
+    }
+}
