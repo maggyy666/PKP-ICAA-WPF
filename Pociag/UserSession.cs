eb4150bacbb7f252da8e445ee0c4a928611859ef
maggyy666 <kczyz20@gmail.com>
2024-06-16 13:14:48 +0200

    Update UserSession class

--- Pociag/UserSession.cs
+++ Pociag/UserSession.cs
@@ -1,24 +1,36 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Pociag
+﻿namespace Pociag
 {
     class UserSession
     {
+        public static int UserId { get; set; }
         public static string Username { get; set; } = "Guest";
+        public static string Email { get; set; } = string.Empty;
+        public static string Password { get; set; } = string.Empty;
+        public static string SelectedDiscount { get; set; } = string.Empty;
+        public static int DiscountId { get; set; }
+
         public static bool IsLoggedIn { get; set; } = false;
-        public static void Login(string username)
+
+        public static void Login(int userId, string username, string email, string password, int discountId, string selectedDiscount)
         {
+            UserId = userId;
             Username = username;
+            Email = email;
+            Password = password;
+            DiscountId = discountId;
+            SelectedDiscount = selectedDiscount;
             IsLoggedIn = true;
         }
+
         public static void Logout()
         {
+            UserId = -1;
             Username = "Guest";
-            IsLoggedIn=false;
+            Email = string.Empty;
+            Password = string.Empty;
+            SelectedDiscount = string.Empty;
+            DiscountId = -1;
+            IsLoggedIn = false;
         }
     }
 }
